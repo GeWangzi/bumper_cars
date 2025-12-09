64a7922c570bbbf7537cc59bdae970882fdc5b6f
Alex Ge <alex_geforce@hotmail.com>
2025-12-09 11:14:32 -0600

    add port env variable

--- server.js
+++ server.js
@@ -59,6 +59,7 @@ io.on("connection", (socket) => {
   });
 });
 
-httpServer.listen(3000, () => {
-  console.log("Server listening on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+httpServer.listen(PORT, () => {
+  console.log("Server listening on PORT", PORT);
 });
